fc446502267557bf8a70a871675012bd77ef571a
sakilk130 <sakilk130@gmail.com>
2022-12-17 00:56:55 +0600

    feat: :sparkles: create searchbar components

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import NavLinks from './NavLinks';
+import SearchBox from './SearchBox';
 
 const Header = () => {
   return (
@@ -21,6 +22,7 @@ const Header = () => {
         </div>
       </div>
       <NavLinks />
+      <SearchBox />
     </header>
   );
 };
